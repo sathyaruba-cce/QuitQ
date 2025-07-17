c703d4a9f6b133525806f73da2aaf2c83e0d9ff6
sathyaruba-cce <107853895+sathyaruba-cce@users.noreply.github.com>
2025-07-17 20:01:48 +0530

    Add files via upload

---
+++ quitq.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE quitq_db;
+use quitq_db;
+show tables;
+select * from product;
+
+
+desc product;
+desc cart_item;
+select * from user;
+select * from cart_item;
+select * from seller;
+select * from orders;
+select * from order_item;
+SELECT email, role FROM user;
